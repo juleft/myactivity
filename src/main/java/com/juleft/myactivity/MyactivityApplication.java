11ecdbc608af392de6bbd7195c482fa557b354f4
Juleft <2463872067@qq.com>
2022-07-24 20:16:17 +0800

    init

---
+++ src/main/java/com/juleft/myactivity/MyactivityApplication.java
@@ -0,0 +1,15 @@
+package com.juleft.myactivity;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyactivityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyactivityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/juleft/myactivity/MyactivityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juleft.myactivity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyactivityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
